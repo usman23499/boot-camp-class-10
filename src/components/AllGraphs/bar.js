2526bb84fdf7dd4931ca54afe6532c8963c07cb9
usman23499 <usamn23499@gmail.com>
2021-01-11 03:51:55 +0500

    Add final

--- src/components/AllGraphs/bar.js
+++ src/components/AllGraphs/bar.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import {Bar} from 'react-chartjs-2';
 
-const data = {
-  
-};
 
 
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,8 +3,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import { fade, makeStyles } from '@material-ui/core/styles';
+
+import {  makeStyles } from '@material-ui/core/styles';
 
 
 const useStyles = makeStyles((theme) => ({
